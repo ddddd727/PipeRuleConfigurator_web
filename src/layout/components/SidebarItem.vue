f0920418cbdfad83bd0db1b738e638612ef16a88
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2025-12-23 15:22:30 +0800

    功能：重构SidebarItem组件，优化子路由显示逻辑，简化代码结构

--- src/layout/components/SidebarItem.vue
+++ src/layout/components/SidebarItem.vue
@@ -1,30 +1,66 @@
 <script setup>
+import { ref } from 'vue'
+
 const props = defineProps({
   item: { type: Object, required: true },
   basePath: { type: String, default: '' }
 })
 
+const onlyOneChild = ref(null)
+
 const resolvePath = (routePath) => {
   if (!routePath) return props.basePath
+  if (/^(https?:|mailto:|tel:)/.test(routePath)) {
+    return routePath
+  }
   const separator = props.basePath.endsWith('/') ? '' : '/'
   return (props.basePath + separator + routePath).replace(/\/+/g, '/')
 }
 
-const hasChildren = (item) => {
-  const showingChildren = item.children?.filter(child => !child.meta?.hidden)
-  return showingChildren && showingChildren.length > 0
+/**
+ * 核心判断逻辑：
+ * 判断是否只有一个需要显示的子路由。
+ * 如果是，我们将渲染这个子路由（作为一级菜单），而不是渲染父级目录。
+ */
+const hasOneShowingChild = (children = [], parent) => {
+  const showingChildren = children.filter(item => {
+    if (item.meta?.hidden) {
+      return false
+    } else {
+      // 暂存该子路由
+      onlyOneChild.value = item
+      return true
+    }
+  })
+
+  // 1. 只有一个子路由 -> 显示该子路由 (例如 /pipe/index)
+  if (showingChildren.length === 1) {
+    return true
+  }
+
+  // 2. 没有子路由 -> 显示父路由自己 (例如 /basic/list，有些写法是把 list 写在 children 里但没有 path)
+  if (showingChildren.length === 0) {
+    onlyOneChild.value = { ...parent, path: '', noShowingChildren: true }
+    return true
+  }
+
+  // 3. 有多个子路由 -> 显示父路由为目录
+  return false
 }
 </script>
 
 <template>
   <template v-if="!item.meta?.hidden">
     
-    <el-menu-item v-if="!hasChildren(item)" :index="resolvePath(item.path)">
-      <el-icon v-if="item.meta?.icon">
-        <component :is="item.meta.icon" />
+    <el-menu-item 
+      v-if="hasOneShowingChild(item.children, item) && (!onlyOneChild.children || onlyOneChild.noShowingChildren) && !item.meta?.alwaysShow"
+      :index="resolvePath(item.path + '/' + onlyOneChild.path)"
+    >
+      <el-icon v-if="onlyOneChild.meta?.icon || (item.meta && item.meta.icon)">
+        <component :is="onlyOneChild.meta?.icon || (item.meta && item.meta.icon)" />
       </el-icon>
       <template #title>
-        <span>{{ item.meta.title }}</span>
+        <span>{{ onlyOneChild.meta?.title }}</span>
       </template>
     </el-menu-item>
 
@@ -33,7 +69,7 @@ const hasChildren = (item) => {
         <el-icon v-if="item.meta?.icon">
           <component :is="item.meta.icon" />
         </el-icon>
-        <span>{{ item.meta.title }}</span>
+        <span v-if="item.meta?.title">{{ item.meta.title }}</span>
       </template>
       
       <sidebar-item
