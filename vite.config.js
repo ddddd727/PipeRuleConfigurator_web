9468271776a2e93f9daf514018153428da27b311
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2026-01-30 09:02:42 +0800

    功能：

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5022',
+        target: 'http://localhost:7051',
         changeOrigin: true
       }
     }
