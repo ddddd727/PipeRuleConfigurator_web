877ab0487d54a963765666ddace5c98cbfa356ec
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2026-03-03 13:33:25 +0800

    功能：

--- src/views/PmcCode.vue
+++ src/views/PmcCode.vue
@@ -852,8 +852,7 @@ const cancelCopyRule = () => {
 </script>
 
 <template>
-  <div class="pmc-page-wrapper">
-    <div class="pmc-container">
+  <div class="pmc-container">
     <!-- Top Section -->
     <transition name="el-zoom-in-top">
       <div class="section-block" v-show="showRulePanel">
@@ -1150,13 +1149,6 @@ const cancelCopyRule = () => {
   </el-dialog>
 </template>
 <style scoped>
-/* 增加 wrapper 样式确保高度撑满 */
-.pmc-page-wrapper {
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-}
-
 .pmc-container {
   display: flex;
   flex-direction: column;
