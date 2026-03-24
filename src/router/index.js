3fdad087d11ff0130633d986221d63f584795998
ddddd727 <77392831+ddddd727@users.noreply.github.com>
2026-03-24 15:00:55 +0800

    功能：

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,10 @@ import pipeRouter from './modules/pipe'
 import libraryRouter from './modules/library'
 import propertyRouter from './modules/property'
 import standardSequenceRouter from './modules/standardsequence'
+import PortalLayout from '@/layout/PortalLayout.vue'
 export const constantRoutes = [
-  // 首页重定向到第一个菜单（字典定义的第一个页面）
-  { path: '/', redirect: '/dict/attribute/piping-class' },
+  // 首页直接进入门户页面，不再跳转到字典页
+  { path: '/', component: PortalLayout, hidden: true },
 
 
   dictRouter,
